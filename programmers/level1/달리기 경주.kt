3edece51058276d24f540f1c287f5078dfc996fd
thomas783 <thomas783789@gmail.com>
2023-09-18 16:55:36 +0900

    feat: add test

---
+++ programmers/level1/달리기 경주.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(players: Array<String>, callings: Array<String>): Array<String> {
+        callings.forEach { calling ->
+            val currentplayers.indexOf(calling)
+        }
+        var answer: Array<String> = arrayOf<String>()
+        return answer
+    }
+}
